b8f47be844955fbff4cdd3247bd9838a7054803a
DmitryZ <dmitriyzagulyaev@gmail.com>
2015-12-13 13:55:54 +0500

    Create qt project

---
+++ Algorithm_Lab3/Algorithm_Lab3/main.cpp
@@ -0,0 +1,10 @@
+#include "algorithm_lab3.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	Algorithm_Lab3 w;
+	w.show();
+	return a.exec();
+}
