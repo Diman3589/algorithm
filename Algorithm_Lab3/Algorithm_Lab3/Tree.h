1c3076698746860c877706183b492c120dba9e1b
DmitryZ <dmitriyzagulyaev@gmail.com>
2015-12-26 15:41:21 +0500

    Task1 v3

---
+++ Algorithm_Lab3/Algorithm_Lab3/Tree.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+struct Data {
+	int key;
+	bool color;
+};
+
+// узел
+struct Node {
+	Data data;
+	struct Node *left;
+	struct Node *right;
+	struct Node *parent;
+};
+
+// дерево
+class Tree {
+public:
+	vector<Node*> listTree;
+public:
+	void BuildTree(int &n);
+	void FillTree();
+	bool CheckTree();
+	void ShowTree();
+	int rb_assert(Node *root);
+	int is_red(Node *root);
+private:
+	string ReadFile();
+public:
+	Tree();
+	~Tree();
+};
